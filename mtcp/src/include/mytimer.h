2b59cc420768b775444d1130c0a2ec4903b36385
cppowboy <cppowboy@outlook.com>
2016-06-14 22:10:04 +0800

    add mytimer

---
+++ mtcp/src/include/mytimer.h
@@ -0,0 +1,53 @@
+/*
+ * mytimer.h
+ *
+ *  Created on: 2016年6月14日
+ *      Author: cppowboy
+ */
+
+#ifndef MTCP_SRC_INCLUDE_MYTIMER_H_
+#define MTCP_SRC_INCLUDE_MYTIMER_H_
+
+#include <rte_cycles.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define APP 0
+#define MTCP 1
+#define PACKETIO 2
+#define TIMERSIZE 3
+#define MAXCORENUMBER 16
+
+#define APP_START 0
+#define MTCP_IN 1
+#define PACKETIO_IN 2
+#define PACKETIO_OUT 3
+#define MTCP_OUT 4
+#define APP_END 5
+
+typedef struct _record
+{
+	long long time;
+	int type;
+	record* next;
+}record;
+
+typedef struct _mytimer
+{
+	int corenumber;
+	long long time[MAXCORENUMBER][TIMERSIZE];
+	long long start[MAXCORENUMBER],end[MAXCORENUMBER];
+	record* head[MAXCORENUMBER];
+}mytimer;
+
+mytimer* getmytimer();
+void addtime(mytimer* pmt,int core,int type,long long t);
+void setstarttime(mytimer* pmt,int core,long long t);
+void setendtime(mytimer* pmt,int core,long long t);
+void setcorenumber(mytimer* pmt,int cn);
+void push(mytimer* pmt,int core,long long time,int type);
+void print(mytimer* pmt);
+void destroytimer(mytimer* pmt);
+void taketime(long long t);
+
+#endif /* MTCP_SRC_INCLUDE_MYTIMER_H_ */
---
+++ mtcp/src/mytimer.c
@@ -0,0 +1,112 @@
+/*
+ * mytimer.c
+ *
+ *  Created on: 2016年6月14日
+ *      Author: cppowboy
+ */
+#include "mytimer.h"
+#include <assert.h>
+#include <string.h>
+
+mytimer* getmytimer()
+{
+	static mytimer* pmt=NULL;
+	if(pmt)
+		return pmt;
+	pmt=(mytimer*)malloc(sizeof(mytimer));
+	assert(pmt!=NULL);
+	return pmt;
+}
+void addtime(mytimer* pmt,int core,int type,long long t)
+{
+	pmt->time[core][type]+=t;
+}
+void setstarttime(mytimer* pmt,int core,long long t)
+{
+	pmt->start[core]=t;
+}
+void setendtime(mytimer* pmt,int core,long long t)
+{
+	pmt->end[core]=t;
+}
+void setcorenumber(mytimer* pmt,int cn)
+{
+	pmt->corenumber=cn;
+	int i,j;
+	for(i=0;i<pmt->corenumber;i++)
+	{
+		pmt->start[i]=0;
+		pmt->end[i]=0;
+		for(j=0;j<TIMERSIZE;j++)
+			pmt->time[i][j]=0;
+		pmt->head[i]=NULL;
+	}
+}
+void push(mytimer* pmt,int core,long long time,int type)
+{
+	record* r=(record*)malloc(sizeof(record));
+	r->time=time;
+	r->type=type;
+	r->next=pmt->head[core];
+	pmt->head[core]=r;
+}
+void print(mytimer* pmt)
+{
+	assert(pmt->corenumber>0);
+	int i;
+	for(i=0;i<pmt->corenumber;i++)
+	{
+		fprintf(stderr,"app %ld;stack %ld;packet %ld\n",
+				pmt->time[i][APP],pmt->time[i][MTCP],pmt->time[i][PACKETIO]);
+	}
+	int core;
+	char fname[20];
+	FILE* fout=NULL;
+	record* cur,nxt;
+	for(core=0;core<pmt->corenumber;core++)
+	{
+		sprintf(fname,"mytimer%d.log",core);
+		fout=fopen(fname,"w");
+		if(!fout)
+		{
+			fprintf(stderr,"failed to open file\n");
+			continue;
+		}
+		cur=nxt=NULL;
+		cur=pmt->head[core];
+		while(cur)
+		{
+			nxt=cur->next;
+			fprintf(fout,"%d event at %ld\n",cur->type,cur->time);
+			cur=nxt;
+		}
+		fclose(fout);
+	}
+	fprintf(stderr,"mytimer print finished\n");
+}
+void destroytimer(mytimer* pmt)
+{
+	record *cur,*nxt;
+	int i;
+	for(i=0;i<pmt->corenumber;i++)
+	{
+		cur=pmt->head[i];
+		while(cur)
+		{
+			nxt=cur->next;
+			free(cur);
+			cur=nxt;
+		}
+	}
+	fprintf(stderr,"destroy finished\n");
+}
+void taketime(long long t)
+{
+	long long start,end=0;
+	start=rte_rdtsc_precise();
+	while(end<start+t)
+		end=rte_rdtsc_precise();
+}
+
+
+
